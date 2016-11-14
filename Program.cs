da1312be99d9491babe61fafdaf331546cf6bc03
PetterC <petterchan89@gmail.com>
2016-11-14 08:11:13 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,66 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
-using System.IO;
 
-
-namespace Client
+namespace Server
 {
     class Program
     {
         static void Main(string[] args)
-        {
 
+        {
             try
             {
-                User user = new User();
-                TcpClient tcpclnt = new TcpClient();
-                Console.WriteLine("Connecting.....");
+                IPAddress ipAd = IPAddress.Parse("10.2.20.50");
 
-                tcpclnt.Connect("10.2.20.50", 8000);
-            
-                Console.WriteLine("Connected");
+                TcpListener myList = new TcpListener(ipAd, 8000);
 
-                user.GetUserName();
+                myList.Start();
 
+                Console.WriteLine("The server is running at port 8000...");
+                Console.WriteLine("The local End point is  :" + myList.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection.....");
 
-                Console.WriteLine();
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
 
-
-                String str = Console.ReadLine();
-                Stream stm = tcpclnt.GetStream();
+                byte[] b = new byte[100];
+                int k = s.Receive(b);
+                Console.WriteLine("Recieved...");
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(b[i]));
 
                 ASCIIEncoding asen = new ASCIIEncoding();
-                byte[] ba = asen.GetBytes(str);
-                Console.WriteLine("Transmitting.....");
-
-                stm.Write(ba, 0, ba.Length);
-
-
-                byte[] bb = new byte[100];
-                int k = stm.Read(bb, 0, 100);
-
-                for (int i = 0; i < k; i++)
-                    Console.Write(Convert.ToChar(bb[i]));
+                s.Send(asen.GetBytes("The string was recieved by the server."));
+                Console.WriteLine("\nSent Acknowledgement");
                 Console.ReadLine();
 
-                tcpclnt.Close();
-            }
+                s.Close();
+                myList.Stop();
 
+            }
             catch (Exception e)
             {
                 Console.WriteLine("Error..... " + e.StackTrace);
-                Console.ReadLine();
+                Console.ReadKey();
+
             }
-
-
-
-
 
         }
     }
 }
-    
